121788ec7894d3d1ae192fce5b9b2368cf9283ff
gh3nt <deihk@lykamspam.pl>
2013-12-18 01:10:50 +0100

    zrobiona cześc klasy ProjektNormy do zrobienia obcinanie numeru projektu

--- Pozyskiwanie_prN/src/pl/c0/la/pozyskiwanie/ProjektNormy.java
+++ Pozyskiwanie_prN/src/pl/c0/la/pozyskiwanie/ProjektNormy.java
@@ -40,7 +40,7 @@ public class ProjektNormy {
 	private boolean akredytacja;
 	
 	//czy jej zakres tematyczny jest zwi¹zany z dzia³alnoœci¹ ITB
-	private boolean zwiazana;
+	private boolean zwiazany;
 	
 	/**
 	 * Konstruktor z za³o¿enia ma przyjmowaæ dane dostêpne na stronie PKN z ogloszeniem o ankiecie powszechnej 
@@ -53,12 +53,94 @@ public class ProjektNormy {
 	 */
 	public ProjektNormy(String numer, String nazwa, String nazwaEN, String zakres, String zakresEN, String koniecAnkiety){
 		this.numer = numer;
+		this.numerKrotki = skrocNumer(numer);
 		this.nazwa = nazwa;
 		this.nazwaEN = nazwaEN;
 		this.zakresEN = zakresEN;
 		this.koniecAnkiety = koniecAnkiety;
-		this.numerKrotki = skrocNumer(numer);
+		
 	}
+	
+	public String getNumer(){
+		return numer;
+	}
+	
+	public String getNumerKrotki(){
+		return numerKrotki;
+	}
+	
+	public String getNazwa(){
+		return nazwa;
+	}
+	
+	public String getNazwaEN(){
+		return nazwaEN;
+	}
+	
+	public String getZakres(){
+		return zakres;
+	}
+	
+	public String getZakresEN(){
+		return zakresEN;
+	}
+	
+	public String getKoniecAnkiety(){
+		return koniecAnkiety;
+	}
+	
+	public void setNrKt(int nrKT){
+		this.nrKT = nrKT;
+	}
+	public int getNrKT(){
+		return nrKT;
+	}
+	
+	/**
+	 * Czy projekt normy jest na liœcie norm zharmonizowanych
+	 * @param b
+	 */
+	public void setZharmonizowana(boolean b){
+		this.zharmonizowana = b;
+	}
+	/**
+	 * Czy projekt normy jest na liœcie norm zharmonizowanych
+	 * @return 
+	 */
+	public boolean getZharmonizowana(){
+		return zharmonizowana;
+	}
+	
+	/**
+	 * Czy projekt normy jest w zakresie akredytacjiITB
+	 * @param b
+	 */
+	public void setAkredytacja(boolean b){
+		this.akredytacja = b;
+	}
+	/**
+	 * Czy projekt normy jest w zakresie akredytacjiITB
+	 * @return 
+	 */
+	public boolean getAkredytacja(){
+		return zharmonizowana;
+	}
+	
+	/**
+	 * czy zakres tematyczny projektu normy jest zwi¹zany z dzia³alnoœci¹ ITB
+	 * @param b
+	 */
+	public void setZwiazany(boolean b){
+		zwiazany = b;
+	}
+	/**
+	 * czy zakres tematyczny projektu normy jest zwi¹zany z dzia³alnoœci¹ ITB
+	 * @return
+	 */
+	public boolean getZwiazany(){
+		return zwiazany;
+	}
+	
 	
 	/**
 	 * Z pe³nego numeru projektu normy (typu prPN-prEN 12345-3:2011/prA1 wybiera tylko numer normy, bez daty czyli np. 12345-3
@@ -67,6 +149,32 @@ public class ProjektNormy {
 	 */
 	public static String skrocNumer(String numer){
 		String numerKrotki = "";
+		
+		
+		//znajdz pierwsz¹ cyfrê w ci¹gu
+		char[] znaki = numer.toCharArray();
+		
+		//poczatek i koniec substringa
+		int pocz;
+		int kon;
+		
+		int i;
+		
+		for(i = 0; i < znaki.length; i++){
+			if (Character.isDigit(znaki[i])){
+				pocz = i;
+				break;
+			}
+		}
+		
+		for(; i < znaki.length; i++){
+			//if (! ((Character.isDigit(znaki[i]) ) )
+		}
+		
+		
+		
+		//wybierz ci¹g od piwerwszej cyfry do dowolnego znaku innego ni¿ cyfra i "-"
+		
 		return numerKrotki;
 	}
 
