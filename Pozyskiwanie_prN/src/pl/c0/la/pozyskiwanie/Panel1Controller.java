d7140a3b34e977fc58525a3b03d9764220b71adc
Luk <Luk@Luk-Komputer>
2013-12-24 13:09:05 +0100

    repeta

---
+++ Pozyskiwanie_prN/src/pl/c0/la/pozyskiwanie/Panel1Controller.java
@@ -0,0 +1,45 @@
+package pl.c0.la.pozyskiwanie;
+
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+
+public class Panel1Controller {
+	
+	//obiekt zarz¹dzaj¹cy operacjami na stronach internetowych
+	WebManager wm;
+	
+	//obiekt zarz¹dzaj¹cy operacjami na plikach
+	FileManager fm;
+	
+	//g³owny kontroler programu
+	MainFrame parent;
+	
+	public Panel1Controller(MainFrame parent){
+		this.parent = parent;
+		wm = new WebManager();
+		fm = new FileManager();
+	}
+	
+	public void otworzStrone(String url){
+		wm.otworzStrone(url);
+	}
+
+	public void otworzPlikExcela(String plik) {
+		fm.otworzPlik(plik);	
+	}
+	
+	/**
+	 * Rozpocznij pobieranie danych z pliku. Wstaw dane do tablicy obiektów ProjektNormy i zmien panel w oknie g³ównym
+	 */
+	public void rozpocznij(String plik){
+		ArrayList<ProjektNormy> listaPN = fm.pobierzProjekty(plik);
+		
+		
+		parent.krok2(listaPN);
+		
+	}
+	
+
+}
